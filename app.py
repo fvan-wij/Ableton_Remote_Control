0028bcad3779c7b3e7baf76871dcc0a9542273e5
Flip van Wijk <fvan-wij@student.codam.nl>
2024-06-28 00:14:19 +0200

    Created a little interface. Video recording now works via MediaRecorder API and triggers simultaneously with Ableton

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template
 import subprocess
 
 app = Flask(__name__)
@@ -9,11 +9,11 @@ def index():
 
 @app.route('/start_recording', methods=['POST'])
 def start_recording():
-    subprocess.run(['python', 'TriggerRecording.py'])
+    subprocess.run(['python', 'record_ableton.py'])
     try:
-        return redirect(url_for('index'))
+        return '', 204
     except Exception as e:
         return f'Error: {str(e)}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
